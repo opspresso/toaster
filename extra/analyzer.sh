915d70e723dec0ac0f7857b3b8e576d86e341fcf
nalbam-ya <jyyu@yanolja.in>
2016-06-29 20:40:14 +0900

    analyzer

--- extra/analyzer.sh
+++ extra/analyzer.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+PHP="/usr/local/php/bin/php"
 PHP_MD="/data/phptool/phpmd/vendor/bin/phpmd"
 
 PROJECT=$1
 WORK="/var/lib/jenkins/jobs/${PROJECT}/workspace"
 
-/usr/local/php/bin/php ${PHP_MD} ${WORK} xml cleancode,codesize,controversial,design,naming,unusedcode --reportfile phpmd.xml | echo 1
+CONFIG="cleancode,codesize,controversial,design,naming,unusedcode"
+
+${PHP} ${PHP_MD} ${WORK} xml ${CONFIG} --reportfile phpmd.xml | echo 1
