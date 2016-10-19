b69d1331e52e3bf7a16d42aea869fdfe016d6715
nalbam-ya <jyyu@yanolja.in>
2016-10-19 13:58:07 +0900

    auth

--- toast.sh
+++ toast.sh
@@ -278,6 +278,9 @@ init() {
         munin)
             init_munin
             ;;
+        tyk)
+            init_tyk
+            ;;
         *)
             self_info
             init_auto
@@ -499,6 +502,18 @@ config_cron() {
 
     echo_bar
     crontab -l
+    echo_bar
+}
+
+init_eip() {
+    # http://docs.aws.amazon.com/cli/latest/reference/ec2/index.html
+    # http://docs.aws.amazon.com/cli/latest/reference/ec2/allocate-address.html
+    # http://docs.aws.amazon.com/cli/latest/reference/ec2/associate-address.html
+    # http://docs.aws.amazon.com/cli/latest/reference/ec2/disassociate-address.html
+    # http://docs.aws.amazon.com/cli/latest/reference/ec2/release-address.html
+
+    echo_bar
+    echo "eip"
     echo_bar
 }
 
@@ -1110,6 +1125,17 @@ init_munin() {
         service_ctl munin-node restart on
 
         touch "${SHELL_DIR}/.config_munin"
+    fi
+}
+
+init_tyk() {
+    if [ ! -f "${SHELL_DIR}/.config_tyk" ]; then
+        echo "init tyk..."
+
+        service_install pygpgme
+
+
+
     fi
 }
 
