ea3d940c82fde3592540e3f5c4aad7d17e670c78
nalbam-ya <jyyu@yanolja.in>
2016-11-25 18:16:19 +0900

    add_env

--- toast.sh
+++ toast.sh
@@ -605,6 +605,11 @@ init_hosts() {
 init_profile() {
     echo "init profile..."
 
+    # TODO remove
+    if [ -f "${HOME}/.bash_profile_toast" ]; then
+        mv -f ${HOME}/.bash_profile_toast ${HOME}/.bash_profile
+    fi
+
     # .bash_toast
     TARGET="${HOME}/.toast_profile"
 
