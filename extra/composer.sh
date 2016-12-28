520c451d63bb7d30b3a100c6dcc9b451b3f6ef8f
nalbam-ya <jyyu@yanolja.in>
2016-12-28 17:02:33 +0900

    local_path

--- extra/composer.sh
+++ extra/composer.sh
@@ -4,18 +4,28 @@ PHP="/usr/bin/php"
 COMPOSER="/usr/local/bin/composer"
 
 PROJECT=$1
-if [ -d "~/.jenkins/workspace/${PROJECT}" ]; then
-    WORK="~/.jenkins/workspace/${PROJECT}"
+if [ "${PROJECT}" == "" ]; then
+    WORK="."
 else
-    if [ -d "/var/lib/jenkins/jobs/${PROJECT}/workspace" ]; then
-        WORK="/var/lib/jenkins/jobs/${PROJECT}/workspace"
+    if [ -d "~/.jenkins/workspace/${PROJECT}" ]; then
+        WORK="~/.jenkins/workspace/${PROJECT}"
+    else
+        if [ -d "/var/lib/jenkins/jobs/${PROJECT}/workspace" ]; then
+            WORK="/var/lib/jenkins/jobs/${PROJECT}/workspace"
+        fi
     fi
 fi
-if [ "WORK" == "" ]; then
+
+if [ "${WORK}" == "" ]; then
     exit 1
 fi
+
 TARGET="${WORK}/src/main/webapp"
 VENDOR="${TARGET}/vendor"
+
+if [ ! -d ${TARGET} ]; then
+    exit 1
+fi
 
 cd ${TARGET}
 
--- extra/swagger.sh
+++ extra/swagger.sh
@@ -4,17 +4,27 @@ PHP="/usr/bin/php"
 SWAGGER="~/.composer/vendor/bin/swagger"
 
 PROJECT=$1
-if [ -d "~/.jenkins/workspace/${PROJECT}" ]; then
-    WORK="~/.jenkins/workspace/${PROJECT}"
+if [ "${PROJECT}" == "" ]; then
+    WORK="."
 else
-    if [ -d "/var/lib/jenkins/jobs/${PROJECT}/workspace" ]; then
-        WORK="/var/lib/jenkins/jobs/${PROJECT}/workspace"
+    if [ -d "~/.jenkins/workspace/${PROJECT}" ]; then
+        WORK="~/.jenkins/workspace/${PROJECT}"
+    else
+        if [ -d "/var/lib/jenkins/jobs/${PROJECT}/workspace" ]; then
+            WORK="/var/lib/jenkins/jobs/${PROJECT}/workspace"
+        fi
     fi
 fi
-if [ "WORK" == "" ]; then
+
+if [ "${WORK}" == "" ]; then
     exit 1
 fi
+
 TARGET="${WORK}/src/main/webapp/application"
 DEST="${WORK}/src/main/webapp/apidoc"
+
+if [ ! -d ${TARGET} ]; then
+    exit 1
+fi
 
 ${PHP} ${SWAGGER} ${TARGET} -o ${DEST}
