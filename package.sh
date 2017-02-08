7cbe719788800a8006c0c0dd72071cfdf4ae4f41
nalbam-ya <jyyu@yanolja.in>
2017-02-08 14:22:34 +0900

    version

--- package.sh
+++ package.sh
@@ -8,7 +8,15 @@ mkdir target
 
 zip -q -r target/toaster extra package *.sh
 
+VERSION=${1}
+if [ "${VERSION}" == "" ]; then
+    VERSION="0.0.0"
+fi
+
+echo "${VERSION}" > target/version.txt
+
 option="--quiet --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers"
 
 aws s3 cp target/toaster.zip s3://repo.toast.sh/release/ ${option}
+aws s3 cp target/version.txt s3://repo.toast.sh/release/ ${option}
 aws s3 cp install.sh s3://repo.toast.sh/release/ ${option}
--- remote.sh
+++ remote.sh
@@ -3,7 +3,7 @@
 ############################################################
 
 function usage() {
-  echo "Usage: $0 {user} {host} {port:22}"
+    echo "Usage: $0 {user} {host} {port:22}"
 }
 
 ############################################################
@@ -19,20 +19,16 @@ CMD=${4}
 
 PARAM1=${5}
 PARAM2=${6}
-PARAM3=${7}
-PARAM4=${8}
-PARAM5=${9}
-PARAM6=${10}
 
 if [ "${HOST}" == "" ]; then
-  usage
-  exit 1
+    usage
+    exit 1
 fi
 
 if [ "${PORT}" == "" ]; then
-  PORT=22
+    PORT=22
 fi
 
-COMMAND="~/toaster/toast.sh ${CMD} ${PARAM1} ${PARAM2} ${PARAM3} ${PARAM4} ${PARAM5} ${PARAM6}"
+COMMAND="~/toaster/toast.sh ${CMD} ${PARAM1} ${PARAM2}"
 
 ssh -t ${USER}@${HOST} -p ${PORT} "${COMMAND}"
