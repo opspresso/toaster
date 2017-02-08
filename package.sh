3508e3b069c6cec607a8766b85830fdfbd5a19a5
nalbam-ya <jyyu@yanolja.in>
2017-02-08 13:56:58 +0900

    grants

--- package.sh
+++ package.sh
@@ -8,4 +8,4 @@ mkdir target
 
 zip -q -r target/toaster extra package *.sh
 
-aws s3 cp target/toaster.zip s3://repo.toast.sh/release/ --quiet
+aws s3 cp target/toaster.zip s3://repo.toast.sh/release/ --quiet --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
