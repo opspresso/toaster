ddc2c0119c6630c38dfe872aa243230174c1c5e4
nalbam-ya <jyyu@yanolja.in>
2017-05-18 17:56:52 +0900

    deploy toast.sh

--- publish.sh
+++ publish.sh
@@ -2,16 +2,16 @@
 
 OPTION="--quiet --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers"
 
-REPO="s3://toast.sh"
-
-# upload
-aws s3 cp install.sh ${REPO}/ ${OPTION}
-aws s3 cp target/toaster.txt ${REPO}/ ${OPTION}
-aws s3 cp target/toaster.zip ${REPO}/ ${OPTION}
-
 REPO="s3://repo.toast.sh"
 
 # upload
 aws s3 cp install.sh ${REPO}/release/ ${OPTION}
 aws s3 cp target/toaster.txt ${REPO}/release/ ${OPTION}
 aws s3 cp target/toaster.zip ${REPO}/release/ ${OPTION}
+
+REPO="s3://toast.sh"
+
+# upload
+aws s3 cp install.sh ${REPO}/ ${OPTION}
+aws s3 cp target/toaster.txt ${REPO}/ ${OPTION}
+aws s3 cp target/toaster.zip ${REPO}/ ${OPTION}
