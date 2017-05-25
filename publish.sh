8d647f914eb54660e0d986ccf5e0756bf8be5daa
nalbam-ya <jyyu@yanolja.in>
2017-05-25 15:07:59 +0900

    circle-ci

--- publish.sh
+++ publish.sh
@@ -3,7 +3,9 @@
 TARGET_PATH="target"
 DEPLOY_PATH="s3://toast.sh/circle"
 
-OPTION="--grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers"
+OPTION=""
+#OPTION="--acl public-read"
+#OPTION="--grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers"
 
 # upload
 aws s3 sync "${TARGET_PATH}" "${DEPLOY_PATH}" ${OPTION}
