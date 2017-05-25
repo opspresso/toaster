64bca1e48c940426aa215596518846aab30ee8a2
nalbam-ya <jyyu@yanolja.in>
2017-05-25 14:40:09 +0900

    circle-ci-aws_s3_cp

--- publish.sh
+++ publish.sh
@@ -3,4 +3,4 @@
 OPTION="--grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers"
 
 # upload
-aws s3 sync target/ s3://toast.sh/circle/ ${OPTION}
+aws s3 cp target/* s3://toast.sh/circle/ ${OPTION}
