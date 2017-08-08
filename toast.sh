bb49f85372e4957691b101d7ade3d7532b10ede5
nalbam-ya <jyyu@yanolja.com>
2017-08-08 10:02:46 +0900

    config local

--- toast.sh
+++ toast.sh
@@ -490,6 +490,10 @@ self_update() {
 }
 
 prepare() {
+    if [ "${PHASE}" == "local" ]; then
+        return
+    fi
+
     service_install "gcc curl wget unzip vim git telnet httpie"
 
     # /data
