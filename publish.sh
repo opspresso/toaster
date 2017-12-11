8bb65ca417f7a40dc93b7e1515926677e6e73f84
nalbam-ya <jyyu@yanolja.com>
2017-12-11 16:49:12 +0900

    CIRCLE_PROJECT_USERNAME

--- publish.sh
+++ publish.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-aws s3 sync target/ s3://toast.sh/ --acl public-read
+CIRCLE_PROJECT_USERNAME="$1"
+
+if [ "${CIRCLE_PROJECT_USERNAME}" == "yanolja" ]; then
+    BUCKET="toast.yanolja.com"
+else
+    BUCKET="toast.sh"
+fi
+
+aws s3 sync target/ s3://${BUCKET}/ --acl public-read
