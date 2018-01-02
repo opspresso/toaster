e5ec726ec2a751d62e6b46ce3c76b13b09fec744
nalbam-ya <jyyu@yanolja.com>
2018-01-02 11:31:06 +0900

    buildspec

--- publish.sh
+++ publish.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-CIRCLE_PROJECT_USERNAME="$1"
-
-if [ "${CIRCLE_PROJECT_USERNAME}" == "yanolja" ]; then
-    BUCKET="toaster.yanolja.com"
-else
-    BUCKET="toast.sh"
-fi
+BUCKET="toast.sh"
 
 aws s3 sync target/ s3://${BUCKET}/ --acl public-read
