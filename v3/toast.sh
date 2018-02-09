f2130778d8ae0a861db78c2362501196d00e48e7
nalbam <skfqka@80>
2018-02-09 11:00:59 +0900

    aws login

--- v3/toast.sh
+++ v3/toast.sh
@@ -557,7 +557,7 @@ publish_docker() {
     echo_ "publish to docker... [${REPOSITORY}]"
 
     if [ "${PARAM2}" == "ECR" ]; then
-        eval $(aws ecr get-login --no-include-email --region ${REGION})
+        aws ecr get-login --no-include-email --region ${REGION}
         #aws ecr create-repository --repository-name ${ARTIFACT_ID}
     fi
 
