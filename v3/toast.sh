f6daf24e52d1fca745343b7d01e46299eabb67bc
nalbam <skfqka@80>
2018-02-20 10:53:21 +0900

    docker_build_latest

--- v3/toast.sh
+++ v3/toast.sh
@@ -553,33 +553,33 @@ publish_docker() {
         error "Not set REPOSITORY."
     fi
 
-    echo_ "publish to docker... [${REPOSITORY}]"
+    echo_ ">> [${REPOSITORY}]"
 
     docker version
 
     pushd target/docker
 
-    echo_ "docker build... [${ARTIFACT_ID}:${VERSION}]"
+    echo_ ">> docker build... [${ARTIFACT_ID}:latest]"
 
-    docker build --rm=false -t ${REPOSITORY}/${ARTIFACT_ID}:${VERSION} .
+    docker build --rm=false -t ${REPOSITORY}/${ARTIFACT_ID}:latest .
 
     docker images
 
     if [ "${PARAM2}" == "ECR" ]; then
-        echo_ "docker login..."
+        echo_ ">> docker login..."
 
         ECR_LOGIN=$(aws ecr get-login --region ${REGION})
         eval ${ECR_LOGIN}
     fi
 
-    echo_ "docker push... [${ARTIFACT_ID}:${VERSION}]"
+    echo_ ">> docker push... [${ARTIFACT_ID}:latest]"
 
-    docker push ${REPOSITORY}/${ARTIFACT_ID}:${VERSION}
+    docker push ${REPOSITORY}/${ARTIFACT_ID}:latest
 
-    #echo_ "docker tag... [${ARTIFACT_ID}:latest]"
+    #echo_ ">> docker tag... [${ARTIFACT_ID}:${VERSION}]"
  
-    #ECR_TAG=$(aws ecr batch-get-image --repository-name ${ARTIFACT_ID} --image-ids imageTag=${VERSION} --query images[].imageManifest --output text)
-    #aws ecr put-image --repository-name ${ARTIFACT_ID} --image-tag latest --image-manifest "${ECR_TAG}"
+    #ECR_TAG=$(aws ecr batch-get-image --repository-name ${ARTIFACT_ID} --image-ids imageTag=latest --query images[].imageManifest --output text)
+    #aws ecr put-image --repository-name ${ARTIFACT_ID} --image-tag ${VERSION} --image-manifest "${ECR_TAG}"
 
     popd
 }
