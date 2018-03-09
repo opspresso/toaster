19f60bc287a8484df10af86a4d75cc4677eb0fbd
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2018-03-09 11:52:06 +0900

    al2

--- openshift/install-node.sh
+++ openshift/install-node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+#sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
 sudo yum-config-manager --enable epel
 sudo yum install -y git nano wget zip zile net-tools docker \
@@ -8,10 +8,10 @@ sudo yum install -y git nano wget zip zile net-tools docker \
     openssl-devel httpd-tools java-1.8.0-openjdk-headless NetworkManager \
     "@Development Tools"
 
-if ! command -v docker > /dev/null; then
-    sudo yum-config-manager --enable rhui-REGION-rhel-server-extras
-    sudo yum install -y docker
-fi
+#if ! command -v docker > /dev/null; then
+#    sudo yum-config-manager --enable rhui-REGION-rhel-server-extras
+#    sudo yum install -y docker
+#fi
 
 sudo systemctl start docker
 sudo systemctl enable docker
