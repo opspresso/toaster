5096ea661db4442d032b4f3b7e66e0f57ea786f8
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2018-03-09 13:13:06 +0900

    sudo

--- openshift/README.md
+++ openshift/README.md
@@ -2,25 +2,23 @@
 
 ## install : 1 master
 ```
-export master_ip="13.125.199.79"
-
+export master_ip="13.125.169.230"
+ 
 scp -i keys/_key_pairs/nalbam-seoul.pem keys/_key_pairs/nalbam-seoul.pem ec2-user@${master_ip}:~/.ssh/id_rsa
-
-ssh -i keys/_key_pairs/nalbam-seoul.pem ec2-user@${master_ip} -t 'curl -s toast.sh/install-v3 | bash'
-ssh -i keys/_key_pairs/nalbam-seoul.pem ec2-user@${master_ip} -t '~/toaster/openshift/install.sh'
+ssh -i keys/_key_pairs/nalbam-seoul.pem ec2-user@${master_ip}
+ 
+curl -s toast.sh/install-v3 | bash
+~/toaster/openshift/install.sh
 ```
 
 ## install : 1 bastion, 1 master, 2 node
 ```
 export bastion_ip="13.125.153.54"
-
+ 
 scp -i keys/_key_pairs/nalbam-seoul.pem keys/_key_pairs/nalbam-seoul.pem ec2-user@${bastion_ip}:~/.ssh/id_rsa
 ssh -i keys/_key_pairs/nalbam-seoul.pem ec2-user@${bastion_ip} -t 'curl -s toast.sh/install-v3 | bash'
 ssh -i keys/_key_pairs/nalbam-seoul.pem ec2-user@${bastion_ip} -t '~/toaster/openshift/install-bastion.sh'
 ```
-
-## console
-* https://console.nalbam.com:8443/
 
 ## reference
 * https://blog.openshift.com/installing-openshift-3-7-1-30-minutes/
---
+++ openshift/install-openshift.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+## see: https://www.youtube.com/watch?v=-OOnGK-XeVY
+
+export SHELL_DIR=$(dirname "$0")
+
+export DOMAIN=${DOMAIN:="$(curl ipinfo.io/ip).nip.io"}
+export USERNAME=${USERNAME:="$(whoami)"}
+export PASSWORD=${PASSWORD:=password}
+export VERSION=${VERSION:="v3.7.1"}
+
+export SCRIPT_REPO=${SCRIPT_REPO:="https://raw.githubusercontent.com/gshipley/installcentos/master"}
+
+export IP="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"
+
+echo "******"
+echo "* Your domain is $DOMAIN "
+echo "* Your username is $USERNAME "
+echo "* Your password is $PASSWORD "
+echo "* OpenShift version: $VERSION "
+echo "******"
+
+yum install -y epel-release
+
+yum install -y git wget zile nano net-tools docker \
+    python-cryptography pyOpenSSL.x86_64 python2-pip \
+    openssl-devel python-devel httpd-tools NetworkManager python-passlib \
+    java-1.8.0-openjdk-headless "@Development Tools"
+
+systemctl | grep "NetworkManager.*running"
+if [ $? -eq 1 ]; then
+	systemctl start NetworkManager
+	systemctl enable NetworkManager
+fi
+
+which ansible || pip install -Iv ansible
+
+[ ! -d openshift-ansible ] && git clone https://github.com/openshift/openshift-ansible.git
+
+cd openshift-ansible && git fetch && git checkout release-3.7 && cd ..
+
+envsubst < ${SHELL_DIR}/hosts > /tmp/hosts
+sudo cp -rf /tmp/hosts /etc/hosts
+
+if [ -z $DISK ]; then
+	echo "Not setting the Docker storage."
+else
+	cp /etc/sysconfig/docker-storage-setup /etc/sysconfig/docker-storage-setup.bk
+
+	echo DEVS=$DISK > /etc/sysconfig/docker-storage-setup
+	echo VG=DOCKER >> /etc/sysconfig/docker-storage-setup
+	echo SETUP_LVM_THIN_POOL=yes >> /etc/sysconfig/docker-storage-setup
+	echo DATA_SIZE="100%FREE" >> /etc/sysconfig/docker-storage-setup
+
+	systemctl stop docker
+
+	rm -rf /var/lib/docker
+	wipefs --all $DISK
+	docker-storage-setup
+fi
+
+systemctl restart docker
+systemctl enable docker
+
+if [ ! -f ~/.ssh/id_rsa ]; then
+	ssh-keygen -q -f ~/.ssh/id_rsa -N ""
+	cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+	ssh -o StrictHostKeyChecking=no root@$IP "pwd" < /dev/null
+fi
+
+export METRICS="True"
+export LOGGING="True"
+
+memory=$(cat /proc/meminfo | grep MemTotal | sed "s/MemTotal:[ ]*\([0-9]*\) kB/\1/")
+
+if [ "$memory" -lt "4194304" ]; then
+	export METRICS="False"
+fi
+
+if [ "$memory" -lt "8388608" ]; then
+	export LOGGING="False"
+fi
+
+curl -o inventory.download $SCRIPT_REPO/inventory.ini
+envsubst < inventory.download > inventory.ini
+ansible-playbook -i inventory.ini openshift-ansible/playbooks/byo/config.yml
+
+htpasswd -b /etc/origin/master/htpasswd ${USERNAME} ${PASSWORD}
+oc adm policy add-cluster-role-to-user cluster-admin ${USERNAME}
+
+systemctl restart origin-master-api
+
+echo "******"
+echo "* Your conosle is https://console.$DOMAIN:8443"
+echo "* Your username is $USERNAME "
+echo "* Your password is $PASSWORD "
+echo "*"
+echo "* Login using:"
+echo "*"
+echo "$ oc login -u ${USERNAME} -p ${PASSWORD} https://console.$DOMAIN:8443/"
+echo "******"
+
+oc login -u ${USERNAME} -p ${PASSWORD} https://console.$DOMAIN:8443/
