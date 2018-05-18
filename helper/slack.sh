04b5acc533261d02aca3ea97f9a68b7f37beafd6
nalbam <me@nalbam.com>
2018-05-18 17:33:20 +0900

    slack

--- helper/slack.sh
+++ helper/slack.sh
@@ -9,41 +9,81 @@ usage() {
     echo " \__ \ | (_| | (__|   < "
     echo " |___/_|\__,_|\___|_|\_\  by nalbam"
     echo "================================================================================"
-    echo " Usage: slack.sh {webhook_url} {channel} {message}"
+    echo " Usage: slack.sh -w {webhook} -n {channel} -i {icon} -u {username} -c {color} -m {message}"
     echo "================================================================================"
 
     exit 1
 }
 
-# ------------
-webhook_url=$1
-if [[ "${webhook_url}" == "" ]]; then
+url=
+channel=
+icon_emoji=
+username=
+color=
+text=
+
+while getopts ":w:c:m:n:u:i:" opt; do
+  case ${opt} in
+    w)
+      url="$OPTARG"
+      ;;
+    n)
+      channel="$OPTARG"
+      ;;
+    c)
+      color="$OPTARG"
+      ;;
+    i)
+      icon_emoji="$OPTARG"
+      ;;
+    u)
+      username="$OPTARG"
+      ;;
+    m)
+      text="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG"
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument."
+      exit 1
+      ;;
+  esac
+done
+
+if [ "${url}" == "" ]; then
     usage
 fi
-
-# ------------
-shift
-channel=$1
-if [[ "${channel}" == "" ]]; then
-    usage
-fi
-
-# ------------
-shift
-text=$*
-
-if [[ "${text}" == "" ]]; then
-    while IFS= read -r line; do
-        text="$text$line\n"
-    done
-fi
-
-if [[ "${text}" == "" ]]; then
+if [ "${text}" == "" ]; then
     usage
 fi
 
 message=$(echo ${text} | sed 's/"/\"/g' | sed "s/'/\'/g")
 
-json="{\"channel\": \"$channel\", \"attachments\":[{\"text\": \"$message\"}]}"
+json="{"
 
-curl -s -d "payload=$json" "$webhook_url"
+if [ "${channel}" != "" ]; then
+    json="$json\"channel\":\"${channel}\","
+fi
+if [ "${icon_emoji}" != "" ]; then
+    json="$json\"icon_emoji\":\"${icon_emoji}\","
+fi
+if [ "${username}" != "" ]; then
+    json="$json\"username\":\"${username}\","
+fi
+
+json="$json\"attachments\":[{"
+
+if [ "${color}" != "" ]; then
+    json="$json\"color\":\"${color}\","
+fi
+
+json="$json\"text\":\"${message}\""
+json="$json}]}"
+
+#echo "url=$url"
+#echo "payload=$json"
+
+curl -s -d "payload=$json" "$url"
