1869e00ec6cd5ccff9f49fa043fa6f059eaf1f80
nalbam <me@nalbam.com>
2018-06-20 15:10:27 +0900

    java

--- helper/bastion.sh
+++ helper/bastion.sh
@@ -14,17 +14,17 @@ echo "==========================================================================
 
 # update
 echo "================================================================================"
-echo "# update "
+echo "# update... "
 sudo yum update -y
 
 # tools
 echo "================================================================================"
-echo "# install tools "
+echo "# install tools... "
 sudo yum install -y git vim telnet jq
 
 # aws-cli
 echo "================================================================================"
-echo "# install awscli "
+echo "# install awscli... "
 pip install --upgrade --user awscli
 aws --version
 
@@ -40,7 +40,7 @@ EOF
 
 # kubectl
 echo "================================================================================"
-echo "# install kubectl "
+echo "# install kubectl... "
 cat <<EOF > kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -56,7 +56,7 @@ kubectl version --client --short
 
 # kops
 echo "================================================================================"
-echo "# install kops "
+echo "# install kops... "
 export VERSION=$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | jq --raw-output '.tag_name')
 curl -sLO https://github.com/kubernetes/kops/releases/download/${VERSION}/kops-linux-amd64
 chmod +x kops-linux-amd64 && sudo mv kops-linux-amd64 /usr/local/bin/kops
@@ -64,28 +64,44 @@ kops version
 
 # helm
 echo "================================================================================"
-echo "# install helm "
+echo "# install helm... "
 export VERSION=$(curl -s https://api.github.com/repos/kubernetes/helm/releases/latest | jq --raw-output '.tag_name')
-curl -sL https://storage.googleapis.com/kubernetes-helm/helm-${VERSION}-linux-amd64.tar.gz | tar xzv
+curl -sL https://storage.googleapis.com/kubernetes-helm/helm-${VERSION}-linux-amd64.tar.gz | tar xz
 sudo mv linux-amd64/helm /usr/local/bin/helm
 helm version --client --short
 
 # jenkins-x
 echo "================================================================================"
-echo "# install jenkins-x "
+echo "# install jenkins-x... "
 export VERSION=$(curl -s https://api.github.com/repos/jenkins-x/jx/releases/latest | jq --raw-output '.tag_name')
-curl -sL https://github.com/jenkins-x/jx/releases/download/${VERSION}/jx-linux-amd64.tar.gz | tar xzv
+curl -sL https://github.com/jenkins-x/jx/releases/download/${VERSION}/jx-linux-amd64.tar.gz | tar xz
 sudo mv jx /usr/local/bin/jx
 jx --version
 
 # terraform
 echo "================================================================================"
-echo "# install terraform "
+echo "# install terraform... "
 export VERSION=$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | jq --raw-output '.tag_name' | cut -c 2-)
 curl -sLO https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_linux_amd64.zip
 unzip terraform_${VERSION}_linux_amd64.zip && rm -rf terraform_${VERSION}_linux_amd64.zip
 sudo mv terraform /usr/local/bin/terraform
 terraform version
+
+# java
+echo "================================================================================"
+echo "# install java... "
+sudo yum remove -y java-1.7.0-openjdk java-1.7.0-openjdk-devel
+sudo yum install -y java-1.8.0-openjdk java-1.8.0-openjdk-devel
+java -version
+
+# maven
+echo "================================================================================"
+echo "# install maven... "
+export VERSION=3.5.3
+curl -sL https://www.apache.org/dist/maven/maven-3/${VERSION}/binaries/apache-maven-${VERSION}-bin.tar.gz | tar xz
+sudo mv apache-maven-${VERSION} /usr/local/
+sudo ln -sf /usr/local/apache-maven-${VERSION}/bin/mvn /usr/local/bin/mvn
+mvn -version
 
 echo "================================================================================"
 echo "# Done. "
