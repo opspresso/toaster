7f5e1a486686d0e37fbf7c91fce37e5f0765b0cc
nalbam <me@nalbam.com>
2018-07-03 13:46:22 +0900

    --quiet

--- publish.sh
+++ publish.sh
@@ -6,6 +6,6 @@ if [ "${REPO}" == "" ]; then
     REPO="toast.sh"
 fi
 
-aws s3 sync target/ s3://${REPO}/ --quiet --acl public-read
+aws s3 sync target/ s3://${REPO}/ --acl public-read
 
 echo "sync to ${REPO}"
