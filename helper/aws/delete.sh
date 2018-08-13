e69c6a53f914fa4001e916f9fb098b376a9b128c
nalbam <me@nalbam.com>
2018-08-13 13:33:46 +0900

    add 'Auto Scaling Groups'

--- helper/aws/delete.sh
+++ helper/aws/delete.sh
@@ -12,13 +12,50 @@ if [ "${YES}" != "yes" ]; then
 fi
 
 REGIONS=/tmp/aws_regions
-INSTANCES=/tmp/aws_ec2_instances
-VOLUMES=/tmp/aws_ec2_volumes
+RESOURCES=/tmp/aws_RESOURCES
 
 aws ec2 describe-regions | grep RegionName | cut -d'"' -f4 > ${REGIONS}
 
+# echo "################################################################################"
+# echo "# ElasticBeanstalk"
+
+# while read REGION; do
+#     echo ">> region : ${REGION}"
+
+#     aws configure set default.region ${REGION}
+
+#     # EB Environments
+#     aws elasticbeanstalk describe-environments | jq '.Environments[] | {ApplicationName,EnvironmentName}' | \
+#         grep EnvironmentName | cut -d'"' -f4 > ${RESOURCES}
+
+#     while read KEY; do
+#         aws elasticbeanstalk delete-environment-configuration --application-name ${KEY} --environment-name ${VAL}
+#     done < ${RESOURCES}
+
+#     aws elasticbeanstalk describe-environments | jq '.Environments[] | {ApplicationName,EnvironmentName}'
+# done < ${REGIONS}
+
 echo "################################################################################"
-echo "# EC2 Instances."
+echo "# Auto Scaling Groups"
+
+while read REGION; do
+    echo ">> region : ${REGION}"
+
+    aws configure set default.region ${REGION}
+
+    # Auto Scaling Groups
+    aws autoscaling describe-auto-scaling-groups | jq '.AutoScalingGroups[] | {AutoScalingGroupName}' | \
+        grep AutoScalingGroupName | cut -d'"' -f4 > ${RESOURCES}
+
+    while read KEY; do
+        aws ec2 delete-auto-scaling-group --auto-scaling-group-name ${KEY}
+    done < ${RESOURCES}
+
+    aws autoscaling describe-auto-scaling-groups | jq '.AutoScalingGroups[] | {AutoScalingGroupName}'
+done < ${REGIONS}
+
+echo "################################################################################"
+echo "# EC2 Instances"
 
 while read REGION; do
     echo ">> region : ${REGION}"
@@ -27,41 +64,42 @@ while read REGION; do
 
     # EC2 Instances ('running', 'pending', 'stopping', 'stopped') ('shutting-down', 'terminated')
     aws ec2 describe-instances | \
-        jq '.Reservations[].Instances[] | {InstanceId: .InstanceId, InstanceType: .InstanceType, State: .State.Name} | select(.State != "terminated")' | \
-        grep InstanceId | cut -d'"' -f4 > ${INSTANCES}
+        jq '.Reservations[].Instances[] | {InstanceId,InstanceType,State:.State.Name} | select(.State != "terminated")' | \
+        grep InstanceId | cut -d'"' -f4 > ${RESOURCES}
 
-    while read ID; do
-        aws ec2 modify-instance-attribute --instance-id ${ID} --no-disable-api-termination
-        aws ec2 terminate-instances --instance-ids ${ID} | grep InstanceId
-    done < ${INSTANCES}
+    while read KEY; do
+        aws ec2 modify-instance-attribute --instance-id ${KEY} --no-disable-api-termination
+        aws ec2 terminate-instances --instance-ids ${KEY} | grep InstanceId
+    done < ${RESOURCES}
 
     aws ec2 describe-instances | \
-        jq '.Reservations[].Instances[] | {InstanceId: .InstanceId, InstanceType: .InstanceType, State: .State.Name}'
+        jq '.Reservations[].Instances[] | {InstanceId,InstanceType,State:.State.Name}'
 done < ${REGIONS}
 
 echo "################################################################################"
-echo "# EBS volumes."
+echo "# EBS Volumes"
 
 while read REGION; do
     echo ">> region : ${REGION}"
 
     aws configure set default.region ${REGION}
 
-#    # EBS volumes
-#    aws ec2 describe-volumes | jq '.Volumes[] | {VolumeId: .VolumeId, State: .State} | select(.State=="in-use")' | \
-#        grep VolumeId | cut -d'"' -f4 > ${VOLUMES}
+#    # EBS Volumes
+#    aws ec2 describe-volumes | jq '.Volumes[] | {VolumeId,State} | select(.State=="in-use")' | \
+#        grep VolumeId | cut -d'"' -f4 > ${RESOURCES}
 #
-#    while read ID; do
-#        aws ec2 detach-volume --volume-id ${ID}
-#    done < ${VOLUMES}
+#    while read KEY; do
+#        aws ec2 detach-volume --volume-id ${KEY}
+#    done < ${RESOURCES}
 
-    aws ec2 describe-volumes | jq '.Volumes[] | {VolumeId: .VolumeId, State: .State}' | grep VolumeId | cut -d'"' -f4 > ${VOLUMES}
+    # EBS Volumes
+    aws ec2 describe-volumes | jq '.Volumes[] | {VolumeId,State}' | grep VolumeId | cut -d'"' -f4 > ${RESOURCES}
 
-    while read ID; do
-        aws ec2 delete-volume --volume-id ${ID}
-    done < ${VOLUMES}
+    while read KEY; do
+        aws ec2 delete-volume --volume-id ${KEY}
+    done < ${RESOURCES}
 
-    aws ec2 describe-volumes | jq '.Volumes[] | {VolumeId: .VolumeId, State: .State}'
+    aws ec2 describe-volumes | jq '.Volumes[] | {VolumeId,State}'
 done < ${REGIONS}
 
 echo "################################################################################"
--- helper/aws/describe.sh
+++ helper/aws/describe.sh
@@ -5,7 +5,29 @@ REGIONS=/tmp/aws_regions
 aws ec2 describe-regions | grep RegionName | cut -d'"' -f4 > ${REGIONS}
 
 echo "################################################################################"
-echo "# EC2 Instances."
+echo "# ElasticBeanstalk"
+
+while read REGION; do
+    echo ">> region : ${REGION}"
+
+    aws configure set default.region ${REGION}
+
+    aws elasticbeanstalk describe-environments | jq '.Environments[] | {ApplicationName,EnvironmentName}'
+done < ${REGIONS}
+
+echo "################################################################################"
+echo "# Auto Scaling Groups"
+
+while read REGION; do
+    echo ">> region : ${REGION}"
+
+    aws configure set default.region ${REGION}
+
+    aws autoscaling describe-auto-scaling-groups | jq '.AutoScalingGroups[] | {AutoScalingGroupName}'
+done < ${REGIONS}
+
+echo "################################################################################"
+echo "# EC2 Instances"
 
 while read REGION; do
     echo ">> region : ${REGION}"
@@ -13,18 +35,18 @@ while read REGION; do
     aws configure set default.region ${REGION}
 
     aws ec2 describe-instances | \
-        jq '.Reservations[].Instances[] | {InstanceId: .InstanceId, InstanceType: .InstanceType, State: .State.Name}'
+        jq '.Reservations[].Instances[] | {InstanceId,InstanceType,State:.State.Name}'
 done < ${REGIONS}
 
 echo "################################################################################"
-echo "# EBS volumes."
+echo "# EBS Volumes"
 
 while read REGION; do
     echo ">> region : ${REGION}"
 
     aws configure set default.region ${REGION}
 
-    aws ec2 describe-volumes | jq '.Volumes[] | {VolumeId: .VolumeId, State: .State}'
+    aws ec2 describe-volumes | jq '.Volumes[] | {VolumeId,State}'
 done < ${REGIONS}
 
 echo "################################################################################"
