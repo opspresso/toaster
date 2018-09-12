b44e1b25350fdafe93405614094e1b1628cb3fd5
nalbam <me@nalbam.com>
2018-09-12 11:52:45 +0900

    new line

--- package.sh
+++ package.sh
@@ -13,8 +13,10 @@ echo "OS_NAME=${OS_NAME}"
 VERSION=$(curl -s https://api.github.com/repos/nalbam/toaster/releases/latest | grep tag_name | cut -d'"' -f4)
 VERSION=$(echo ${VERSION:-v0.0.0} | perl -pe 's/^(([v\d]+\.)*)(\d+)(.*)$/$1.($3+1).$4/e')
 
-echo "VERSION=${VERSION}"
 printf "${VERSION}" > target/VERSION
+
+echo "VERSION=${VERSION}"
+echo
 
 # 755
 find ./** | grep [.]sh | xargs chmod 755
@@ -34,16 +36,16 @@ fi
 pushd draft
 tar -czf ../target/dist/draft.tar.gz *
 popd
+echo
 
 # target/dist/helper.tar.gz
 pushd helper
 tar -czf ../target/dist/helper.tar.gz *
 popd
+echo
 
 # target/helper/
 cp -rf helper/* target/helper/
 
 # target/
 cp -rf web/* target/
-
-ls -al target
