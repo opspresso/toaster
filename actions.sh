2055ce73ae94f48a76562a016452a86b5786a4de
nalbam <me@nalbam.com>
2019-09-19 15:59:25 +0900

    gpg

--- actions.sh
+++ actions.sh
@@ -83,7 +83,7 @@ _build() {
         _error "not found VERSION"
     fi
 
-    TMP_STRING="--${STRING}--$STRING--"
+    TMP_STRING="$(gpg --decrypt --cipher-algo AES256 ${STRING})"
     _result "STRING=${TMP_STRING}"
 
     _result "USERNAME=${USERNAME}"
