cb71761d4c48780bbc07ef72e6260a1c1447fdcb
nalbam <me@nalbam.com>
2019-09-20 10:51:18 +0900

    --data @

--- actions.sh
+++ actions.sh
@@ -190,7 +190,7 @@ _release_assets() {
             -H "${AUTH_HEADER}" \
             -H "${CONTENT_TYPE_HEADER}" \
             -H "${CONTENT_LENGTH_HEADER}" \
-            -F "file1=@${FILEPATH}" \
+            --data "@${FILEPATH}" \
             ${URL}
     done < ${LIST}
 }
