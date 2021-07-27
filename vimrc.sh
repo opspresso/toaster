7e985aca7db68361d7bae06d1b0e627b637109e8
nalbam <me@nalbam.com>
2021-07-27 10:19:56 +0900

    vimrc

--- vimrc.sh
+++ vimrc.sh
@@ -2,17 +2,16 @@ if has("syntax")
 	syntax on
 endif
 
-set hlsearch
-set nu
 set autoindent
-set ts=4
-set sts=4
 set cindent
+set fileencodings=utf8,euc-kr
+set hlsearch
 set laststatus=2
+set ruler
 set shiftwidth=4
 set showmatch
 set smartcase
-set smarttab
 set smartindent
-set ruler
-set fileencodings=utf8,euc-kr
+set smarttab
+set sts=4
+set ts=4
