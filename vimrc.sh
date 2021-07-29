bb99c55db5c9ab377acf7f0241d8e2ee2537148b
nalbam <me@nalbam.com>
2021-07-29 16:15:42 +0900

    vimrc

--- vimrc.sh
+++ vimrc.sh
@@ -2,8 +2,6 @@ if has("syntax")
 	syntax on
 endif
 
-set autoindent
-set cindent
 set fileencodings=utf8,euc-kr
 set hlsearch
 set laststatus=2
@@ -11,7 +9,3 @@ set ruler
 set shiftwidth=4
 set showmatch
 set smartcase
-set smartindent
-set smarttab
-set sts=4
-set ts=4
